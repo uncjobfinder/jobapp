c6d0173297f166d5daa301708c6df3fb45aa833c
Charlie Bowden <charles.bowden@gmail.com>
2017-11-09 20:07:49 -0500

    Firebase added to JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBSabjKGEF0vNtXN9iWRh-TycBywbF8uS0",
+    authDomain: "jobfinder-c4a9b.firebaseapp.com",
+    databaseURL: "https://jobfinder-c4a9b.firebaseio.com",
+    projectId: "jobfinder-c4a9b",
+    storageBucket: "jobfinder-c4a9b.appspot.com",
+    messagingSenderId: "214976884404"
+  };
+  
+  firebase.initializeApp(config);
